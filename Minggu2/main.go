7f7b9cef9426fd9a680f396a1860588571c11c17
Dharma Chandra Viriya <dcviriya313@gmail.com>
2026-03-03 08:32:12 +0700

    create is kabisat function

--- Minggu2/main.go
+++ Minggu2/main.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
-	//
+	var tahun int = 0
+	var isKabisat bool = false
+
+	fmt.Print("Masukkan tahun\t: ")
+	fmt.Scanln(&tahun)
+
+	if (tahun%400 == 0 || tahun%4 == 0) && tahun%100 != 0 {
+		isKabisat = true
+	}
+
+	fmt.Printf("Kabisat\t\t: %t\n", isKabisat)
 }
