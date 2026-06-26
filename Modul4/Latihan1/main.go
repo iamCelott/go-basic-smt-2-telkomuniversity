64fc48031aaab35e43814c755e2bba98ed0cc314
Dharma Chandra Viriya <dharmachandraviriya@Dharmas-MacBook-Pro.local>
2026-06-26 18:21:29 +0700

    fix modul 4

--- Modul4/Latihan1/main.go
+++ Modul4/Latihan1/main.go
@@ -1,27 +1,40 @@
+// Dharma Chandra Viriya
+// S1IF-13-02
+// 109082500052
 package main
 
 import "fmt"
 
-func factorial(n int) int {
-	if n == 0 {
-		return 1
+func factorial(n int, hasil *int) {
+	*hasil = 1
+	for i := 1; i <= n; i++ {
+		*hasil *= i
 	}
-	return n * factorial(n-1)
 }
 
-func permutation(n, r int) int {
-	return factorial(n) / factorial(n-r)
+func permutation(n, r int, hasil *int) {
+	var fn, fnr int
+	factorial(n, &fn)
+	factorial(n-r, &fnr)
+	*hasil = fn / fnr
 }
 
-func combination(n, r int) int {
-	return factorial(n) / (factorial(r) * factorial(n-r))
+func combination(n, r int, hasil *int) {
+	var fn, fr, fnr int
+	factorial(n, &fn)
+	factorial(r, &fr)
+	factorial(n-r, &fnr)
+	*hasil = fn / (fr * fnr)
 }
 
 func main() {
 	var a, b, c, d int
-
+	var p1, k1, p2, k2 int
 	fmt.Scanln(&a, &b, &c, &d)
-
-	fmt.Printf("%d %d\n", permutation(a, c), combination(a, c))
-	fmt.Printf("%d %d\n", permutation(b, d), combination(b, d))
+	permutation(a, c, &p1)
+	combination(a, c, &k1)
+	permutation(b, d, &p2)
+	combination(b, d, &k2)
+	fmt.Printf("%d %d\n", p1, k1)
+	fmt.Printf("%d %d\n", p2, k2)
 }
