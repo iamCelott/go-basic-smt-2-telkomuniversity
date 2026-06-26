1a8f0322ffd14d2fa2b6b68416b7f3967d9b22b5
Dharma Chandra Viriya <dharmachandraviriya@Dharmas-MacBook-Pro.local>
2026-06-26 17:29:45 +0700

    fix modul 12

--- Modul12/Latihan2/main.go
+++ Modul12/Latihan2/main.go
@@ -1,35 +1,44 @@
+// Dharma Chandra Viriya
+// S1IF-13-02
+// 109082500052
 package main
 
 import "fmt"
 
 func main() {
-	var arr = [21]int{}
+	var arr [21]int
 	totalVote := 0
 	validVote := 0
-	ketua := 0
-	wakilKetua := 0
 	for {
 		var suara int
 		fmt.Scan(&suara)
+
 		if suara == 0 {
 			break
 		}
-		if suara > 0 && suara <= 20 {
+		totalVote++
+		if suara >= 1 && suara <= 20 {
 			validVote++
 			arr[suara]++
 		}
-		totalVote++
 	}
-	fmt.Println("Suara masuk: ", totalVote)
-	fmt.Println("Suara sah: ", validVote)
-	for i := 1; i <= validVote; i++ {
+	ketua := 1
+	for i := 2; i <= 20; i++ {
 		if arr[i] > arr[ketua] {
 			ketua = i
 		}
-		if arr[i] > arr[wakilKetua] && i != ketua {
-			wakilKetua = i
+	}
+	wakil := 0
+	for i := 1; i <= 20; i++ {
+		if i == ketua {
+			continue
+		}
+		if wakil == 0 || arr[i] > arr[wakil] {
+			wakil = i
 		}
 	}
-	fmt.Println("Ketua RT: ", ketua)
-	fmt.Println("Wakil Ketua: ", wakilKetua)
+	fmt.Println("Suara masuk:", totalVote)
+	fmt.Println("Suara sah:", validVote)
+	fmt.Println("Ketua RT:", ketua)
+	fmt.Println("Wakil Ketua:", wakil)
 }
